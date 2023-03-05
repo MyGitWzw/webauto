939b49813be67b1dd49b22f8eb65f2c06f19654d
wzw-auto-repo <1391982095@qq.com>
2023-03-05 17:33:05 +0800

    Lines starting
    update GitBasic.java add testGit() method

--- src/test/java/hwonline/testing/GitBasic.java
+++ src/test/java/hwonline/testing/GitBasic.java
@@ -3,5 +3,10 @@ package hwonline.testing;
 public class GitBasic {
     public static void main(String[] args) {
         System.out.println("This is class can be added to github");
+
+    }
+
+    public  void testGit(){
+        System.out.println("This is a test method to verify");
     }
 }
